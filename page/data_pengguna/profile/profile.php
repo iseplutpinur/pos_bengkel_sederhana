bdfb7e81d9efd8665d5640be7f808f7fae527d67
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2020-09-30 12:22:26 +0700

    Profile saya

--- page/data_pengguna/profile/profile.php
+++ page/data_pengguna/profile/profile.php
@@ -1,7 +1,13 @@
 <?php
-if (isset($_SESSION['user']['id'])) {
-    $id_user      = $_SESSION['user']['id'];
-    $user         = query("SELECT * FROM tb_user INNER JOIN tb_user_level ON tb_user.id_level = tb_user_level.id_level WHERE tb_user.id_user='$id_user'");
+if (isset($_SESSION['user']['id'])) :
+    $id_user       = $_SESSION['user']['id'];
+    $passlama      = true;
+    $passbaru      = true;
+    $pass          = [];
+    $pass['lama']  = (isset($_POST['user_password_lama'])) ? $_POST['user_password_lama'] : '';
+    $pass['baru']  = (isset($_POST['user_password_baru'])) ? $_POST['user_password_baru'] : '';
+    $pass['baru1'] = (isset($_POST['user_password_baru1'])) ? $_POST['user_password_baru1'] : '';
+    $user          = query("SELECT * FROM tb_user INNER JOIN tb_user_level ON tb_user.id_level = tb_user_level.id_level WHERE tb_user.id_user='$id_user'");
     if ($user) $user = $user[0];
 
     if (isset($_POST['ubah-data'])) {
@@ -42,7 +48,7 @@ if (isset($_SESSION['user']['id'])) {
                 setAlert('Berhasil..! ', 'Data berhasil diubah..', 'success');
                 echo '<script type = "text/javascript">window.location.href = "' . $_baseurl . '";</script>';
             } else {
-                setAlert('Gagal..! ', 'Data gagal diubah..', 'success');
+                setAlert('Gagal..! ', 'Data gagal diubah..', 'danger');
                 echo '<script type = "text/javascript">window.location.href = "' . $_baseurl . '";</script>';
             }
         } else {
@@ -60,60 +66,84 @@ if (isset($_SESSION['user']['id'])) {
                 setAlert('Berhasil..! ', 'Data berhasil diubah..', 'success');
                 echo '<script type = "text/javascript">window.location.href = "' . $_baseurl . '";</script>';
             } else {
-                setAlert('Gagal..! ', 'Data gagal diubah..', 'success');
+                setAlert('Gagal..! ', 'Data gagal diubah..', 'danger');
                 echo '<script type = "text/javascript">window.location.href = "' . $_baseurl . '";</script>';
             }
         }
     }
 
-    // { ["id_user"]=> string(1) "2" ["id_level"]=> string(1) "2" ["user_username"]=> string(5) "admin" ["user_password"]=> string(60) "$2y$10$BK4K1rN9bqeGcNvoCeJ1jO2IKJID95viYE.x8FgZDxM9F4crr.N2y" ["user_nama"]=> string(14) "Isep Lutpi Nur" ["user_alamat"]=> string(1) "0" ["user_gender"]=> string(9) "Laki-Laki" ["user_tanggal_lahir"]=> string(10) "0000-00-00" ["user_nik"]=> string(1) "0" ["user_no_telepon"]=> string(1) "0" ["user_foto"]=> string(23) "admin_5f65e86a67474.jpg" ["active"]=> string(1) "1" ["level_title"]=> string(5) "Admin" }
-} else {
-    die;
-}
-?>
-<div class="row">
-    <div class="col-md-3">
-        <!-- Profile Image -->
-        <div class="card card-primary card-outline">
-            <div class="card-body box-profile">
-                <div class="text-center">
-                    <img class="profile-user-img img-fluid img-circle" src="assets/images/user_profile/<?= $user['user_foto']; ?>" alt="User profile picture">
-                </div>
-                <h3 class="profile-username text-center"><?= $user['user_nama']; ?></h3>
-                <p class="text-muted text-center"><?= $user['level_title']; ?></p>
-                <ul class="list-group list-group-unbordered mb-3">
-                    <li class="list-group-item">
-                        <b>Username</b> <a class="float-right"><?= $user['user_username']; ?></a>
-                    </li>
-                    <li class="list-group-item">
-                        <b>Tgl Lahir</b> <a class="float-right"><?= $user['user_tanggal_lahir']; ?></a>
-                    </li>
-                    <li class="list-group-item">
-                        <b>Jenis Kelamin</b> <a class="float-right"><?= $user['user_gender']; ?></a>
-                    </li>
-                    <li class="list-group-item">
-                        <b>No Telepon</b> <a class="float-right"><?= $user['user_no_telepon']; ?></a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.card-body -->
-        </div>
-        <!-- /.card -->
-    </div>
+    if (isset($_POST['ubah-password'])) {
+        // cek password lama
+        if (password_verify($_POST['user_password_lama'], $user['user_password'])) {
+            $user_password_baru  = $_POST['user_password_baru'];
+            $user_password_baru1 = $_POST['user_password_baru1'];
+            // cek password baru apakah sama atau tidak
+            if ($user_password_baru == $user_password_baru1) {
+                // acak dulu password sebelum dimasukan kedalam database
+                $pw_hash = password_hash($user_password_baru, PASSWORD_DEFAULT);
+                $querybuilder = "UPDATE `tb_user` SET  `user_password` = '$pw_hash' WHERE `tb_user`.`id_user`='$id_user'";
 
-    <!-- /.col -->
-    <div class="col-md-9">
-        <!-- About Me Box -->
-        <div class="card">
-            <div class="card-header p-2">
-                <ul class="nav nav-pills">
-                    <li class="nav-item"><a class="nav-link active" href="#data-diri" data-toggle="tab">Data Diri</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#ubah-password" data-toggle="tab">Ubah Password</a></li>
-                </ul>
-            </div><!-- /.card-header -->
-            <div class="card-body">
-                <div class="tab-content">
-                    <div class="active tab-pane" id="data-diri">
+                // cek apakah berhasil diganti atau tidak
+                if ($koneksi->query($querybuilder)) {
+                    setAlert('Berhasil..! ', 'Data berhasil diubah..', 'success');
+                    echo '<script type = "text/javascript">window.location.href = "' . $_baseurl . '";</script>';
+                } else {
+                    setAlert('Gagal..! ', mysqli_error($koneksi), 'danger');
+                    echo '<script type = "text/javascript">window.location.href = "' . $_baseurl . '";</script>';
+                }
+            } else $passbaru = false;
+        } else $passlama = false;
+    }
+?>
+    <div class="row">
+        <div class="col-md-3">
+            <!-- Profile Image -->
+            <div class="card card-primary card-outline">
+                <div class="card-body box-profile">
+                    <div class="text-center">
+                        <img class="profile-user-img img-fluid img-circle" src="assets/images/user_profile/<?= $user['user_foto']; ?>" alt="User profile picture">
+                    </div>
+                    <h3 class="profile-username text-center"><?= $user['user_nama']; ?></h3>
+                    <p class="text-muted text-center"><?= $user['level_title']; ?></p>
+                    <ul class="list-group list-group-unbordered mb-3">
+                        <li class="list-group-item">
+                            <b>Username</b> <a class="float-right"><?= $user['user_username']; ?></a>
+                        </li>
+                        <li class="list-group-item">
+                            <b>Tgl Lahir</b> <a class="float-right"><?= $user['user_tanggal_lahir']; ?></a>
+                        </li>
+                        <li class="list-group-item">
+                            <b>Jenis Kelamin</b> <a class="float-right"><?= $user['user_gender']; ?></a>
+                        </li>
+                        <li class="list-group-item">
+                            <b>No Telepon</b> <a class="float-right"><?= $user['user_no_telepon']; ?></a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+        </div>
+
+        <!-- /.col -->
+        <div class="col-md-9">
+            <!-- About Me Box -->
+            <div class="card">
+                <div class="card-header p-2">
+                    <ul class="nav nav-pills">
+                        <?php if (!$passbaru || !$passlama) : ?>
+                            <li class="nav-item"><a class="nav-link" href="#data-diri" data-toggle="tab">Data Diri</a></li>
+                            <li class="nav-item"><a class="nav-link active" href="#ubah-password" data-toggle="tab">Ubah Password</a></li>
+                        <?php else : ?>
+                            <li class="nav-item"><a class="nav-link active" href="#data-diri" data-toggle="tab">Data Diri</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#ubah-password" data-toggle="tab">Ubah Password</a></li>
+                        <?php endif; ?>
+                    </ul>
+                </div><!-- /.card-header -->
+                <div class="card-body">
+                    <div class="tab-content">
+                        <?php if (!$passbaru || !$passlama) echo '<div class="tab-pane" id="data-diri">';
+                        else echo '<div class="active tab-pane" id="data-diri">'; ?>
                         <form action="" method="post" enctype="multipart/form-data">
                             <div class="row">
                                 <div class="col-md-4">
@@ -191,7 +221,7 @@ if (isset($_SESSION['user']['id'])) {
                                     </div>
                                 </div>
                                 <div class="row">
-                                    <div class="col-md-12 pl-3 pb-3 pt-1">
+                                    <div class="col-md-12 pl-3 pb-2">
                                         <!-- pake button tidak bisa di submit -->
                                         <input class="btn btn-primary" type="submit" name="ubah-data" value="Ubah Data Diri"></input>
                                     </div>
@@ -200,87 +230,68 @@ if (isset($_SESSION['user']['id'])) {
                         </form>
                     </div>
                     <!-- /.tab-pane -->
-                    <div class="tab-pane" id="ubah-password">
-                        <!-- The timeline -->
-                        <form role="form" id="quickForm">
-                            <div class="card-body">
+                    <?php if (!$passbaru || !$passlama) echo '<div class="tab-pane active" id="ubah-password">';
+                    else echo '<div class="tab-pane" id="ubah-password">'; ?>
+                    <!-- The timeline -->
+                    <form action="" method="post">
+                        <div class="row">
+                            <div class="col-md">
                                 <div class="form-group">
-                                    <label for="exampleInputEmail1">Email address</label>
-                                    <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputPassword1">Password</label>
-                                    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                                </div>
-                                <div class="form-group mb-0">
-                                    <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" name="terms" class="custom-control-input" id="exampleCheck1">
-                                        <label class="custom-control-label" for="exampleCheck1">I agree to the <a href="#">terms of service</a>.</label>
-                                    </div>
+                                    <label for="user_password_lama">Password Lama</label>
+                                    <?php if ($passlama) : ?>
+                                        <input type="password" class="form-control" id="user_password_lama" name="user_password_lama" placeholder="Password Lama" value="<?php echo $pass['lama']; ?>" required="">
+                                    <?php else : ?>
+                                        <input type="password" class="form-control is-invalid" id="user_password_lama" name="user_password_lama" placeholder="Password Lama" value="<?php echo $pass['lama']; ?>" required="">
+                                        <span id="user_password_lama-error" class="error invalid-feedback">Password yang anda masukan salah..!</span>
+                                    <?php endif; ?>
                                 </div>
                             </div>
-                            <!-- /.card-body -->
-                            <div class="card-footer">
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                        <div class="row">
+                            <div class="col-md">
+                                <div class="form-group">
+                                    <label for="user_password_baru">Password Baru</label>
+                                    <?php if ($passbaru) : ?>
+                                        <input type="password" class="form-control" id="user_password_baru" name="user_password_baru" placeholder="Password Baru" value="<?php echo $pass['baru']; ?>" required="">
+                                    <?php else : ?>
+                                        <input type="text" class="form-control is-invalid" id="user_password_baru" name="user_password_baru" placeholder="Password Baru" value="<?php echo $pass['baru']; ?>" required="">
+                                    <?php endif; ?>
+                                </div>
                             </div>
-                        </form>
-                    </div>
-                    <!-- /.tab-pane -->
+                        </div>
+                        <div class="row">
+                            <div class="col-md">
+                                <div class="form-group">
+                                    <label for="user_password_baru1">Ualngi Password</label>
+                                    <?php if ($passbaru) : ?>
+                                        <input type="password" class="form-control" id="user_password_baru1" name="user_password_baru1" placeholder="Ualngi Password" value="<?php echo $pass['baru1']; ?>" required="">
+                                    <?php else : ?>
+                                        <input type="text" class="form-control is-invalid" id="user_password_baru1" name="user_password_baru1" placeholder="Ulangi Password Baru" value="<?php echo $pass['baru1']; ?>" required="">
+                                        <span id="user_password_baru1-error" class="error invalid-feedback">Password yang anda masukan tidak sama denan password baru..!</span>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md">
+                                <div class="col-md-12 my-2 pb-1">
+                                    <!-- pake button tidak bisa di submit -->
+                                    <input class="btn btn-primary" type="submit" name="ubah-password" value="Ubah Data Diri"></input>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <!-- /.tab-content -->
-            </div><!-- /.card-body -->
+            </div>
         </div>
-        <!-- /.card -->
     </div>
-    <!-- /.col -->
-</div>
-<script type="text/javascript">
-    function labelFileFoto(data) {
-        // manual soalnya labelnya gk berfungsi
-        document.querySelector('#label-foto').innerText = data.files[0].name;
-    }
-    $(document).ready(function() {
-        $.validator.setDefaults({
-            submitHandler: function() {
-                alert("Form successful submitted!");
-            }
-        });
-        $('#quickForm').validate({
-            rules: {
-                email: {
-                    required: true,
-                    email: true,
-                },
-                password: {
-                    required: true,
-                    minlength: 5
-                },
-                terms: {
-                    required: true
-                },
-            },
-            messages: {
-                email: {
-                    required: "Please enter a email address",
-                    email: "Please enter a vaild email address"
-                },
-                password: {
-                    required: "Please provide a password",
-                    minlength: "Your password must be at least 5 characters long"
-                },
-                terms: "Please accept our terms"
-            },
-            errorElement: 'span',
-            errorPlacement: function(error, element) {
-                error.addClass('invalid-feedback');
-                element.closest('.form-group').append(error);
-            },
-            highlight: function(element, errorClass, validClass) {
-                $(element).addClass('is-invalid');
-            },
-            unhighlight: function(element, errorClass, validClass) {
-                $(element).removeClass('is-invalid');
-            }
-        });
-    });
-</script>
+    </div>
+    </div>
+    <script type="text/javascript">
+        function labelFileFoto(data) {
+            // manual soalnya labelnya gk berfungsi
+            document.querySelector('#label-foto').innerText = data.files[0].name;
+        }
+    </script>
+
+<?php endif; ?>
